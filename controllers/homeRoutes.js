052b676725ede5e16aff5cafcbde6ac4d7467002
David Keim <dmk252@cornell.edu>
2023-03-19 20:45:25 -0400

    modified get route to show data from database

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const { User } = require('../models');
 
 router.get('/', async (req, res) => {
-  res.render('home');
+  const firstUser = await User.findByPk(1);
+  console.log(firstUser.dataValues);
+  return res.render('home', firstUser.dataValues);
 });
 
 module.exports = router;
