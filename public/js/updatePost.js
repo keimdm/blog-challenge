f6f2f9a0612d48c0dd7c7832971ceb6e830c4ffc
David Keim <dmk252@cornell.edu>
2023-03-22 02:14:51 -0400

    added update function

--- public/js/updatePost.js
+++ public/js/updatePost.js
@@ -3,7 +3,7 @@ const updatePostHandler = async (event) => {
     console.log("pressed update post button");
     const postTitle = document.querySelector('#post-update-title').value.trim();
     const postContent = document.querySelector('#post-update-content').value.trim();
-    const postID = Number(event.target.children[1].children[0].id);
+    const postID = Number(event.target.children[2].children[0].classList[2]);
     const response = await fetch(`/dashboard/post/${postID}/update/submit`, {
         method: 'PUT',
         body: JSON.stringify({ postTitle, postContent }),
