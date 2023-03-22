d54fa4a62770216a8404f7adcbc857c0bbf90a6d
David Keim <dmk252@cornell.edu>
2023-03-21 22:29:11 -0400

    home page comment view

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
   console.log(req.session);
@@ -27,5 +28,38 @@ router.get('/', async (req, res) => {
     loggedIn: req.session.loggedIn
   });
 });
+
+router.get('/post/:id', withAuth, async (req, res) => {
+  console.log("look at post details");
+  console.log(req.params.id);
+  let postData = await Post.findByPk(req.params.id);
+  postData = postData.get({plain: true});
+  let rawCommentData = await Comment.findAll({
+    where: {
+      post_id: req.params.id,
+    },
+    include: [{ model: User }],
+    attributes: {
+      include: [
+        [
+          sequelize.literal(
+            '(SELECT name FROM user WHERE user.id = comment.user_id)'
+          ),
+          'username',
+        ],
+      ],
+    },
+  });
+  let commentData = [];
+  for (i = 0; i < rawCommentData.length; i++) {
+    commentData.push(rawCommentData[i].get({ plain: true }));
+  }
+  return res.render('post-comment', {
+    postData,
+    commentData,
+    loggedIn: req.session.loggedIn
+  });
+});
+
 
 module.exports = router;
